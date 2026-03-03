715ffd9dd093ed442a83b967fc85e37a3efa42df
Rafael Rossatto <rafarossatto25@gmail.com>
2026-03-03 10:08:43 -0300

    add filters

--- src/features/data_frame.py
+++ src/features/data_frame.py
@@ -76,7 +76,13 @@ class DF(pd.DataFrame):
         This method calculates the time it take for an orden to be 
         aproved, by calculeting the diferrence between the order_aproved_at and order purchase_timestamp columns.
         '''
-        time_to_approved = self[['order_id', 'customer_id', 'order_purchase_timestamp', 'order_approved_at']].copy()
+
+
+        df_filtred = self[self['order_status'] != 'canceled'].copy()
+
+        # Selecionar colunas relevantes e remover NAs
+        time_to_approved = df_filtred[['order_id', 'customer_id', 'order_purchase_timestamp', 'order_approved_at']].copy()
+        time_to_approved = time_to_approved.dropna(subset=['order_approved_at', 'order_purchase_timestamp'])
         time_to_approved['order_purchase_timestamp'] = pd.to_datetime(time_to_approved['order_purchase_timestamp'])
         time_to_approved['order_approved_at'] = pd.to_datetime(time_to_approved['order_approved_at'])
         time_to_approved['time_to_aproved'] = time_to_approved['order_approved_at'] - time_to_approved['order_purchase_timestamp']
@@ -88,7 +94,11 @@ class DF(pd.DataFrame):
         This method calculetes the time it takes for an order to be delivered,
         by calculating the difference between the order_delivered_customer_dat and order_purchase_timestamp columns.
         '''
-        transit_time = self[['order_id', 'customer_id','order_delivered_carrier_date','order_delivered_customer_date']].copy()
+
+        df_filtred = self[self['order_status'] != 'canceled'].copy()
+        
+        transit_time = df_filtred[['order_id', 'customer_id', 'order_delivered_carrier_date', 'order_delivered_customer_date']].copy()
+        transit_time = transit_time.dropna(subset=['order_delivered_carrier_date', 'order_delivered_customer_date'])
         transit_time['order_delivered_customer_date'] = pd.to_datetime(transit_time['order_delivered_customer_date'])
         transit_time['order_delivered_carrier_date'] = pd.to_datetime(transit_time['order_delivered_carrier_date'])
         transit_time['transit_time'] = transit_time['order_delivered_customer_date'] - transit_time['order_delivered_carrier_date']
@@ -102,7 +112,11 @@ class DF(pd.DataFrame):
         and the estimate delivery time, calculating the difference between the order_delivered_customer_dat 
         and order_estimated_delivery_dat columns.
         '''
-        comparation_time = self[['order_id', 'customer_id','order_estimated_delivery_date','order_delivered_customer_date']].copy()
+
+        df_filtred = self[self['order_status'] != 'canceled'].copy()
+        
+        comparation_time = df_filtred[['order_id', 'customer_id','order_estimated_delivery_date','order_delivered_customer_date']].copy()
+        comparation_time = comparation_time.dropna(subset=['order_estimated_delivery_date','order_delivered_customer_date'])
         comparation_time['order_delivered_customer_date'] = pd.to_datetime(comparation_time['order_delivered_customer_date'])
         comparation_time['order_estimated_date'] = pd.to_datetime(comparation_time['order_estimated_delivery_date'])
         comparation_time['comparation_time'] = comparation_time['order_estimated_date'] - comparation_time['order_delivered_customer_date']
@@ -116,7 +130,11 @@ class DF(pd.DataFrame):
         by calculating the difference between the order_delivered_customer_dat 
         and order_purchase_timestamp columns.
         '''
-        total_time = self[['order_id', 'customer_id','order_purchase_timestamp','order_delivered_customer_date']].copy()
+
+        df_filtred = self[self['order_status'] != 'canceled'].copy()
+        
+        total_time = df_filtred[['order_id', 'customer_id','order_purchase_timestamp','order_delivered_customer_date']].copy()
+        total_time = total_time.dropna(subset=['order_purchase_timestamp','order_delivered_customer_date'])
         total_time['order_delivered_customer_date'] = pd.to_datetime(total_time['order_delivered_customer_date'])
         total_time['order_purchase_timestamp'] = pd.to_datetime(total_time['order_purchase_timestamp'])
         total_time['total_time'] = total_time['order_delivered_customer_date'] - total_time['order_purchase_timestamp']
@@ -125,11 +143,21 @@ class DF(pd.DataFrame):
         total_time.to_csv(file_path, index=False)
         
  
-    def save_data(self,path):
+    def save_data(self, path, output = None):
         '''
         This method saves the processed data, using the other methods.
         '''
-        self._time_to_approved(path)
-        self._transit_time(path)
-        self._comparation_time(path)
-        self._total_time(path)
+        
+        if output == "time_to_approved":
+            self._time_to_approved(path)
+        elif output == "transit_time":
+            self._transit_time(path)
+        elif output == "comparation_time":
+            self._comparation_time(path)
+        elif output == "total_time":
+            self._total_time(path)
+        else:
+            self._time_to_approved(path)
+            self._transit_time(path)
+            self._comparation_time(path)
+            self._total_time(path)
--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@ from data_code.load_data import LoadData
 from features.data_frame import DF
 
 def main():
-    # Exemplo 1: Uso básico
     path_in = '../data/raw/olist_orders_dataset.csv'
     path_out = "../data/data_process"
     
@@ -10,7 +9,6 @@ def main():
         loader = LoadData(path_in)
         df = loader.load() 
         df.save_data(path_out)
-        #print(df.head())
 
     except FileNotFoundError:
         print("Deu erro...")
